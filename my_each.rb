6a4f1c0cf564f0fe1a0d106ac2c973eeef10ca09
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 14:43:33 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,10 @@
 def my_each(array) # put argument(s) here
   if block_yielded?
-    
+    i = 0
+    while i < array.length
+      yield = array[i]
+      i += 1
+    end
   else
     "Hey! No block was yielded!"
   end
